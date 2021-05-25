a0804d54d9ad1a87178ad06327edc66cde5741ff
Ross White <rosswhite@outlook.com>
2021-05-25 20:53:11 +0100

    Api routes structure

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+router.post('/', (req, res) => {
+});
+
+router.get('/:id', (req, res) => {
+});
+
+router.get('/:id/posts', (req, res) => {
+});
+
+module.exports = router;
