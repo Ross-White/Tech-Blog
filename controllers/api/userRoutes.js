e73e90e8a6e2cab0b0fedc5cc1857dd6bdda1de4
Ross White <rosswhite@outlook.com>
2021-05-27 15:40:22 +0100

    Signup working

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
+const { User} = require('../../models');
 
-router.post('/', (req, res) => {
-    //Create user
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
+            res.status(200).json(userData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
 
 router.get('/:id', (req, res) => {
