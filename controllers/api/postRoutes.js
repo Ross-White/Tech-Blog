792ccee126cf0b004509277123340e7f1faf45bc
Ross White <rosswhite@outlook.com>
2021-05-27 18:40:27 +0100

    Create new post working

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
+const { Post } = require('../../models');
 
-router.post('/', (req, res) => {
-    //Create post
+router.post('/', async (req, res) => {
+    try {
+        const postData = await Post.create(req.body);
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
 
 router.get('/', (req, res) => {
