18654420b1e66f93c173277bf201592c1b6026cc
Ross White <rosswhite@outlook.com>
2021-05-27 17:46:19 +0100

    login working

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,21 +1,49 @@
 const router = require('express').Router();
-const { User} = require('../../models');
+const { User } = require('../../models');
 
 router.post('/', async (req, res) => {
-    try {
-        const userData = await User.create(req.body);
-            res.status(200).json(userData);
-    } catch (err) {
-        res.status(400).json(err);
+  try {
+    const userData = await User.create(req.body);
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+    console.log(userData);
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
     }
+    const validPassword = await userData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
-    //Get user by id
+  //Get user by id
 });
 
 router.get('/:id/posts', (req, res) => {
-    //Get posts by user id
+  //Get posts by user id
 });
 
 module.exports = router;
